a0f0e2644dcf341cddc9e4f37df08df448021774
AndreAlcantara <andrealcantara0720gmail.com>
2019-02-17 17:50:47 -0500

    added products to cart

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
 # Create your models here.
 
 '''
@@ -12,6 +14,23 @@ class Food(models.Model):
     description = models.CharField(max_length = 300)
     price = models.FloatField(default = 0)
     image = models.ImageField(upload_to='food_images/')
+
+'''
+/--------
+  USER
+--------/
+'''
+User = get_user_model()
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    food = models.ManyToManyField(Food, blank=True)
+
+def post_save_profile_create(sender, instance, created, *args, **kwargs):
+    if created:
+        Profile.objects.get_or_create(user=instance)
+    
+post_save.connect(post_save_profile_create, sender=settings.AUTH_USER_MODEL)
 
 '''
 /--------------
@@ -27,6 +46,7 @@ class OrderItem(models.Model):
 
 class Order(models.Model):
     ref_code = models.CharField(max_length=15)
+    owner = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
     is_ordered = models.BooleanField(default=False)
     items = models.ManyToManyField(OrderItem)
     date_ordered = models.DateTimeField(auto_now=True)
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,13 +1,53 @@
+from __future__ import unicode_literals
 from django.shortcuts import render
-from .models import Food, Order, OrderItem
+from .models import Food, Order, OrderItem, Profile
 from django.shortcuts import render, redirect, get_object_or_404
+from django.conf import settings
+from django.contrib import messages
+import datetime
+from datetime import date
+import random
+import string
 # Create your views here.
+
+'''
+/-------
+  FOOD
+-------/
+'''
 def food(request):
     foods = Food.objects
-    return render(request, 'restaurants/food.html', {'foods': foods})
+    filtered_orders = Order.objects.filter(owner=request.user.profile, is_ordered=False)
+    current_order_products = []
+    if filtered_orders.exists():
+        user_order = filtered_orders[0]
+        user_order_items = user_order.items.all()
+        current_order_products = [food.food for food in user_order_items]
+
+    context = {
+        'foods': foods,
+        'current_order_products': current_order_products
+
+    }
+
+    return render(request, 'restaurants/food.html', context)
 
 def home(request):
     return render(request, 'restaurants/home.html')
+
+'''
+/-------
+  USER
+-------/
+'''
+def my_profile(request):
+    my_user_profile = Profile.objects.filter(user=request.user).first()
+    my_orders = Order.objects.filter(is_ordered=True, owner= my_user_profile)
+    context = {
+        'my_orders': my_orders
+    }
+
+    return render(request, 'profile.html', context )
 
 
 '''
@@ -15,14 +55,31 @@ def home(request):
  SHOPPING CART
 --------------/
 '''
+def generate_order_id():
+    date_str = date.today().strftime('%Y%m%d')[2:] + str(datetime.datetime.now().second)
+    rand_str = "".join([random.choice(string.digits) for count in range(3)])
 
 def add_to_cart(request, **kwargs):
+
+    user_profile = get_object_or_404(Profile, user=request.user)
     food = Food.objects.filter(id=kwargs.get('item_id', '')).first()
+
+    if food in request.user.profile.food.all():
+        messages.info(request, 'Passed the quantity limit')
+        return redirect('food')
 
     order_item, status = OrderItem.objects.get_or_create(food=food)
 
-    return redirect('/food/')
+    user_order, status = Order.objects.get_or_create(owner=user_profile, is_ordered=False)
+    user_order.items.add(order_item)
 
+    if status:
+
+        user_order.ref_code = generate_order_id()
+        user_order.save()
+
+    messages.info(request, 'Item added to cart')
+    return redirect('/food/')
 def cart(request, **kwargs):
     return render(request, 'restaurants/cart.html')
 
